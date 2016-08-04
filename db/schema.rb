cef68bd0b900646b78c0d0ab79bcc741b67a2d34
zrsmith <zrs3141592@gmail.com>
2016-08-04 18:13:02 +0000

    Trying to resolve db issue with adding user_id to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804011207) do
+ActiveRecord::Schema.define(version: 20160804174411) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "articles" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",    null: false
